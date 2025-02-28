dff0185c859f76b4495d97a4160b152985ed2304
MihawkJ <153190093+MihawkJ@users.noreply.github.com>
2025-02-28 04:54:02 +0300

    a4
    tasarımdaki bazı şeyler fixlendi ama sadece bazıları

--- anmfrom/frontend/src/pages/animelist.js
+++ anmfrom/frontend/src/pages/animelist.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./andet.css";
 
 const AnimeList = () => {
   const [animes, setAnimes] = useState([]);
